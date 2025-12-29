ad6f9364c36f79e8736809b5b6b6e4d7760b94b8
AnishRN <anish.17175@sakec.ac.in>
2025-12-29 15:08:18 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- prototype.py
+++ prototype.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import tempfile
 import json
 import joblib
-API_KEY = st.secrets["OPENAI_API_KEY"]
+API_KEY = st.secrets["OPENWEATHER_API_KEY"]
 @st.cache_resource
 def load_xgb_model():
     return joblib.load("models/xgb_model.joblib")
@@ -747,4 +747,5 @@ elif page == "Historical Data":
         file_name="pollution_history_report.pdf",
         mime="application/pdf"
     )
+
 
